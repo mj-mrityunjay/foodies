61907f5a687de9eba756cdc4c527fb3906813f30
Mrityunjay Kumar <mrityunjay.k@hcl.com>
2020-10-23 20:57:01 +0530

    Cleanup till menu

--- pilets/foodies-login-pilet/src/index.tsx
+++ pilets/foodies-login-pilet/src/index.tsx
@@ -1,23 +1,8 @@
 import * as React from 'react';
 import { PiletApi } from 'foodies';
 import { Link } from "react-router-dom";
-import Header from './components/common/Header';
-import Footer from './components/common/Footer';
-import Index from './components/Index';
-import Offers from './components/Offers';
-import MyAccount from './components/MyAccount';
-import List from './components/List';
-import NotFound from './components/NotFound';
-import Thanks from './components/Thanks';
-import Extra from './components/Extra';
 import Login from './components/Login';
 import Register from './components/Register';
-import TrackOrder from './components/TrackOrder';
-import Invoice from './components/Invoice';
-import Checkout from './components/Checkout';
-import Detail from './components/Detail';
-import App from './App';
-import { settings } from 'cluster';
 
 let api: PiletApi = undefined;
 export function setSharedData(name) {
